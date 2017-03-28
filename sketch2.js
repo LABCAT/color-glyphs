f1a524374bdbc58e224ba77c52e0134da88dc486
Shane Watters <shane.watters.nz@gmail.com>
2017-03-28 15:29:23 +1300

    initial work on the creation of my interpolations

--- sketch2.js
+++ sketch2.js
@@ -28,23 +28,75 @@ function draw_shape(column, row, size, cur_color) {
   }
   else if (row === 1) {
     // saturation
-    var rect_width = map(column, 0, 4, 10, 80);
-    rect(half_size-rect_width/2, half_size-20, rect_width, 40, 20);
+    // var rect_width = map(column, 0, 4, 10, 80);
+    // rect(half_size-rect_width/2, half_size-20, rect_width, 40, 20);
+    var positions = {
+      'x1': [6, 35.5, 65, 94.5, 120, 120, 120, 120, 94.5, 65, 35.5, 6, 0, 0, 0, 0], 
+      'x3': [25.5, 55, 84.5, 114, 120, 120, 120, 120, 114, 84.5, 55, 25.5, 0, 0, 0, 0],
+      'y1': [0, 0, 0, 0, 6, 35.5, 65, 94.5, 120, 120, 120, 120, 94.5, 65, 35.5, 6],
+      'y3': [0, 0, 0, 0, 25.5, 55, 84.5, 114, 120, 120, 120, 120, 114, 84.5, 55, 25.5]
+    }
+    var scaleMultiplier = map(column, 0, 4, 4, 0);
+    noStroke();
+    fill(255, 255, 255, 47);
+    ellipse(60, 60, 25 * scaleMultiplier, 25 * scaleMultiplier);
+    fill(255);
+    
+    
+    //draw circle in the center
+    ellipse(60, 60, 15 * scaleMultiplier, 15 * scaleMultiplier);
+    
+    fill(0);
+    ellipse(60, 60, 10 * scaleMultiplier, 10 * scaleMultiplier);
+    
+    
+    fill(255, 255, 255, 47);
+    //draw 16 triangles from the center of the square to the edge 
+    for($i = 0; $i < 16; $i++){
+      triangle(positions['x1'][$i], positions['y1'][$i], 60, 60, positions['x3'][$i], positions['y3'][$i]);
+    }
   }
   else {
     // lightness
-    var fill_col = map(column, 0, 4, 30, 90);
-    fill(fill_col);
-    rect(half_size-rect_width/2, half_size-20, rect_width, 40, 20);
+    // var fill_col = map(column, 0, 4, 30, 90);
+    // fill(fill_col);
+    // rect(half_size-rect_width/2, half_size-20, rect_width, 40, 20);
+    //x and y positions for the 16 triangles
+    var positions = {
+      'x1': [6, 35.5, 65, 94.5, 120, 120, 120, 120, 94.5, 65, 35.5, 6, 0, 0, 0, 0], 
+      'x3': [25.5, 55, 84.5, 114, 120, 120, 120, 120, 114, 84.5, 55, 25.5, 0, 0, 0, 0],
+      'y1': [0, 0, 0, 0, 6, 35.5, 65, 94.5, 120, 120, 120, 120, 94.5, 65, 35.5, 6],
+      'y3': [0, 0, 0, 0, 25.5, 55, 84.5, 114, 120, 120, 120, 120, 114, 84.5, 55, 25.5]
+    }
+    var opacityLevel = [15, 31, 47, 63, 79];
+    noStroke();
+    fill(255, 255, 255, opacityLevel[column]);
+    ellipse(60, 60, 25, 25);
+    fill(255);
+    
+    
+    //draw circle in the center
+    ellipse(60, 60, 15, 15);
+    
+    fill(0);
+    ellipse(60, 60, 10, 10);
+    
+    
+    fill(255, 255, 255, opacityLevel[column]);
+    //draw 16 triangles from the center of the square to the edge 
+    for($i = 0; $i < 16; $i++){
+      triangle(positions['x1'][$i], positions['y1'][$i], 60, 60, positions['x3'][$i], positions['y3'][$i]);
+    }
   }
 }
 
 // some examples of how to specify a base color
 // var my_color = "#d24632"
 // var my_color = "rgb(245, 225, 50)"
-var my_color = "rgb(20%, 47%, 67%)"
+//var my_color = "rgb(20%, 47%, 67%)"
+var my_color = "rgb(210, 70, 50)"
 
-var shapes_should_draw = false;
+var shapes_should_draw = true;
 
 // draw five colors and then five glyphs
 function draw () {
@@ -91,7 +143,7 @@ function draw () {
 
       strokeWeight(2);
       stroke(0);
-      fill(bg_color);
+      fill(0);
       var curx = xdiff/2 + xstep * x + 10;
       var cury = ydiff/2 + ystep * y + 10;
       rect(curx, cury, size, size);
