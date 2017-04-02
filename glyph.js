2e4c51411646942143b0eb68a490ac82b6668b0e
Shane Watters <shane.watters.nz@gmail.com>
2017-04-02 18:05:19 +1200

    switching functoinality of saturation and hue dimensions and altering new hue functionality so that there are actually 360 different hues available

--- glyph.js
+++ glyph.js
@@ -4,62 +4,40 @@
  * use p5.js to draw a round grawscale glpyh within the bounding box
  */ 
 function gray_glyph(values, size) {
-  // replace this with your own version
-
-  // map brightness to large circle shade
-  var color1 = map(values[2], 0, 100, 10, 70)
-  //stroke(color1);
   fill(0);
   var center = size/2;
   ellipse(center, center, size);
 
-  // inner size is set to 30%
-  var inner_size = 0.2 + 0.4 * 0.3;
-  var s3 = size * inner_size;
-  // inner color based on saturation
-  var color2 = map(values[1], 0, 100, color1+20, 240)
-  fill(color2);
-  stroke(color2);
-
   //hue dimension
-  var hueMin = map(values[0], 0, 360, center, 0);
-  var hueMax = map(values[0], 0, 360, center, size);
+  var hueDegree = floor(values[0]);
+  var hueModulo = (hueDegree % 12) * 4;
+  var hue = map(hueDegree, 0, 360, size, 0 + size/16);
   
   //saturation dimension
-  var saturation = map(values[1], 0, 100, size, 0)
-
+  var saturationMin = map(values[1], 0, 100, center, 0);
+  var saturationMax = map(values[1], 0, 100, center, size);
+  
   //hue dimension
   var brightnessMin = map(values[2], 0, 100, center, (0 + size/8 + size/32));
   var brightnessMax = map(values[2], 0, 100, center, (size - size/8 - size/32));
   
 
 
+  stroke(255);
+  
   //outer circle
-  fill(255, 255, 255, 47);
-  ellipse(center, center, saturation);
+  fill(255, 255, 255, 47 + hueModulo);
+  ellipse(center, center, hue);
 
   //middle circle
-  fill(255, 255, 255, 159);
-  ellipse(center, center, saturation / 2);
+  fill(255, 255, 255, 159 + hueModulo);
+  ellipse(center, center, hue / 2);
 
   //inner circle
   fill(0);
-  ellipse(center, center, saturation / 4);
-  
-  //print('values = ' + values);
-  //print('values[0] = ' + values[0]);
-  // hue controls left/right shift
-  var shift_frac = (values[0] - 180.0) / 180.0;
-  //print('shift_frac = ' + shift_frac);
-  var max_shift = 0.5 * (size - s3);
-  //print('max_shift = ' + max_shift);
-  var x_shift = shift_frac * max_shift;
-  //ellipse(center + x_shift, center, s3);  
+  ellipse(center, center, hue / 4);
 
-  //translate to the center of the square
-  //translate(100, 100);
-  var s4 = center - s3;
-  //x and y positions for the 16 triangles
+  //x and y positions for the 8 triangles
   var positions = {
     'x1': [
             center - size/32,
@@ -84,19 +62,19 @@ function gray_glyph(values, size) {
     'x2': [
             center,
             brightnessMax,
-            hueMax,
+            saturationMax,
 			brightnessMax,
             center,
             brightnessMin,
-            hueMin,
+            saturationMin,
             brightnessMin
           ],
     'y2': [
-            hueMin,
+            saturationMin,
             brightnessMin,
             center,
 			brightnessMax,
-            hueMax,
+            saturationMax,
             brightnessMax,
             center,
             brightnessMin
@@ -123,14 +101,9 @@ function gray_glyph(values, size) {
           ]
   }
    
-
-  //draw 16 circles, rotating a 16th each time
-  //print('center = ' + center);
-  //print("  x_shift = " + x_shift);
   fill(255, 255, 255, 127);
-  //fill(255, 0, 0);
   noStroke(); 
-  for($i = 0; $i < 16; $i++){
+  for($i = 0; $i < 8; $i++){
     triangle(positions['x1'][$i], positions['y1'][$i], positions['x2'][$i], positions['y2'][$i], positions['x3'][$i], positions['y3'][$i]);
   }
 }
